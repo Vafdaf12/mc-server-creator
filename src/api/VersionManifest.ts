a3d908ee663086f420f6e72cadf318ea2fe5bb75
vafdaf12 <vafdaf11@gmail.com>
2023-01-17 22:32:34 +0200

    Implement Version Manifest abstraction

---
+++ src/api/VersionManifest.ts
@@ -0,0 +1,51 @@
+import got from "got";
+
+const URL_MANIFEST = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+
+import {
+    MCVersionSummary,
+    ReleaseType,
+    MCVersion,
+    MCVersionManifest
+} from "../types";
+
+class VersionManifest {
+    private versions: MCVersionSummary[] = [];
+    public latestRelease: string = "";
+    public latestSnapshot: string = "";
+
+    get all() {
+        return this.versions.map(v => v.id);
+    }
+
+    get releaseTypes() {
+        return Object.values(ReleaseType);
+    }
+
+    byId(id: string): MCVersionSummary | undefined {
+        return this.versions.find(v => v.id === id);
+    }
+
+    byRelease(release: ReleaseType): string[] {
+        return this.versions.filter(v => v.type === release).map(v => v.id);
+    }
+
+    async fetchAll() {
+        const json: MCVersionManifest = await got.get(URL_MANIFEST).json();
+
+        this.latestRelease = json.latest.release;
+        this.latestSnapshot = json.latest.snapshot;
+        this.versions = json.versions;
+    }
+
+    async fetchInfo(id: string): Promise<MCVersion | undefined> {
+        const v = this.byId(id);
+        if (!v) return;
+
+        const data: MCVersion = await got(v.url).json();
+
+        return data;
+    }
+}
+
+export default new VersionManifest();
