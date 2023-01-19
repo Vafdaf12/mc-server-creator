2faf37b74c5408462001146bed712ac1f3efc269
vafdaf12 <vafdaf11@gmail.com>
2023-01-19 17:00:59 +0200

    feat: add function to await events

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
 import { Choice } from "prompts";
+import { EventEmitter } from "events";
 
 export const choiceFromValue = (value: string): Choice => ({
     value,
     title: value
 });
+
+/**
+ * An async way to wait for an event to be emitted
+ * @param e The object that emits the event
+ * @param ev The event to be emitted
+ * @returns A promise of the payload of the event
+ */
+export async function waitForEvent<T = void>(emitter: EventEmitter, event: string): Promise<T> {
+    return new Promise(resolve => emitter.once(event, resolve));
+}
