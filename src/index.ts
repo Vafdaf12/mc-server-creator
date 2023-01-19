b1545b97038b44fbacc11d5946c8ca1a54dcb234
vafdaf12 <vafdaf11@gmail.com>
2023-01-19 22:37:14 +0200

    feat: add support for downloading files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,47 @@
 import VersionManifest from "./api/VersionManifest";
 
 import prompts, { Choice } from "prompts";
+import { SingleBar } from "cli-progress";
 
 import { ReleaseType } from "./types";
 import { choiceFromValue } from "./utils";
+import format from "./util/format";
 
-const TEMPLATES: Choice[] = [
-    choiceFromValue("vanilla"),
-    choiceFromValue("fabric")
-];
+import fs from "fs";
+import got from "got";
+import { pipeline } from "stream/promises";
+
+const TEMPLATES: Choice[] = ["vanilla", "fabric"].map(choiceFromValue);
+
+/**
+ * Displays a progress message while downloading a file from
+ * the given URL
+ * @param url The URL to download from
+ * @param path The file to download to
+ * @param downloadLabel The label given to the progress bar while downloading
+ */
+async function downloadFile(url: string, path: fs.PathLike, downloadLabel: string) {
+    const urlStream = got.stream.get(url);
+    const fileStream = fs.createWriteStream(path);
+
+    const bar = new SingleBar({
+        hideCursor: null,
+        stopOnComplete: true,
+        gracefulExit: true,
+        format: format.download
+    });
+    bar.start(0, urlStream.downloadProgress.total || 0, {
+        label: downloadLabel
+    });
+
+
+    urlStream.on("downloadProgress", ({ transferred, total }) => {
+        bar.setTotal(total);
+        bar.update(transferred);
+    });
+
+    await pipeline(urlStream, fileStream);
+}
 
 async function main() {
     await VersionManifest.fetchAll();
