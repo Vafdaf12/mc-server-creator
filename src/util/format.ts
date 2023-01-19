28fbb53a24ba8f83d9ebd02560cf91abfb639f59
vafdaf12 <vafdaf11@gmail.com>
2023-01-19 22:47:33 +0200

    change: aggregate byte count format to seperate function

--- src/util/format.ts
+++ src/util/format.ts
@@ -5,24 +5,29 @@ import figures from "./figures";
 const FLOAT_DECIMALS = 2;
 const THRESHOLD_MB = 1e6;
 
+function formatByteCount(n: number, places: number = FLOAT_DECIMALS): string {
+    const divFactor = n <= THRESHOLD_MB ? 1e3 : 1e6;
+    const suffix = n <= THRESHOLD_MB ? 'kB' : 'MB';
+
+    const amount = (n / divFactor).toFixed(places);
+    return `${amount} ${suffix}`
+
+}
+
 export default {
     download: (options: Options, params: Params, payload: any): string => {
         const isComplete = params.value >= params.total;
 
         const prefix = isComplete ? kleur.green(figures.tick) : kleur.dim(figures.ellipsis);
 
-        const suffix = params.total <= THRESHOLD_MB ? 'kB' : 'MB';
-
-        const divFactor = params.total <= THRESHOLD_MB ? 1e3 : 1e6;
-
-        const value = (params.value / divFactor).toFixed(FLOAT_DECIMALS);
-        const total = (params.total / divFactor).toFixed(FLOAT_DECIMALS);
+        const value = formatByteCount(params.value);
+        const total = formatByteCount(params.total);
 
         if (isComplete) {
             // Text for completed downloaded
-            return `${prefix} ${kleur.bold(payload.label)}   ` + kleur.green(`Downloaded ${total} ${suffix}`);
+            return `${prefix} ${kleur.bold(payload.label)}   ` + kleur.green(`Downloaded ${total}`);
         } else {
-            return `${prefix} ${payload.label}   ${value} / ${total} ${suffix}`;
+            return `${prefix} ${payload.label}   ${value} / ${total}`;
         }
     }
 }
